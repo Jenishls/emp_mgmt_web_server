e3b051d0bef2311a7c7d325613d6ef38a61b15e5
jenishls <jlshrestha90@gmail.com>
2019-05-16 08:43:05 +0545

    change route bug

--- routes/route_employee.js
+++ routes/route_employee.js
@@ -1,9 +1,9 @@
 const routes = require('express').Router();
 const handlers = require('../handlers/employeeHandler')
 
-routes.post('/contacts',handlers.insertEmployee)
-routes.get('/contacts',handlers.getEmployee)
-routes.put('/contacts',handlers.updateEmployee)
-routes.delete('/contacts',handlers.deleteEmployee)
+routes.post('/',handlers.insertEmployee)
+routes.get('/',handlers.getEmployee)
+routes.put('/',handlers.updateEmployee)
+routes.delete('/',handlers.deleteEmployee)
 
 module.exports = routes
